805d84349454de12c53fa1d342da31f478da3038
Razack <57121998+wsxed1230@users.noreply.github.com>
2024-02-22 22:00:33 +0900

    아니 이게 왜 빠졌지

---
+++ Assets/Script/MobAi/Mob.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mob : MonoBehaviour
+{
+    //변수 영역
+    #region Properties / Field
+    //private 변수 영역
+    #region Private
+    //스킬들 저장될 배열
+    //private Skill[4] mySkills
+    #endregion
+    //protected 변수 영역
+    #region protected
+
+    #endregion
+    //Public 변수영역
+    #region public
+
+    #endregion
+    //이벤트 함수들 영역
+    #region Event
+    #endregion
+    #endregion
+
+    //public 함수들 영역
+    #region PublicMethod
+    #endregion
+
+    //private 함수들 영역
+    #region PrivateMethod
+    #endregion
+
+    //이벤트가 일어났을때 실행되는 On~~함수
+    #region EventHandler
+
+    #endregion
+
+
+    //유니티 함수들 영역
+    #region MonoBehaviour
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    #endregion
+
+
+}
