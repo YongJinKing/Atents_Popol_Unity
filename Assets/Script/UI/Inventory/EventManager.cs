1af681a55c16b04641495f8863eb2ad7cce6bc3d
YongJinKing <dydwls120@gmail.com>
2024-02-28 23:22:32 +0900

    0228인벤토리 및 메인UI 추가
    아이템이 들어있는 슬롯만 선택해서 인벤토리 정보 볼 수 있게 만들고 추가적으로 메인UI를 통해 다양한 페이지를 열 수 있게만듬

--- Assets/Script/UI/Inventory/EventManager.cs
+++ Assets/Script/UI/Inventory/EventManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Microsoft.Unity.VisualStudio.Editor;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
@@ -8,13 +9,12 @@ using UnityEngine.UI;
 public class EventManager : MonoBehaviour
 {
     
-
+    public GameObject SmithInventory;//
+    public GameObject gameCanvas;//게임 
+    public GameObject MainUI;//메인 UI
+    public GameObject UserPanel;
     
-
-    public GameObject SmithInventory;//?�벤?�리 ?�브?�트 ?�당
-    public GameObject gameCanvas;//게임 캔버???�당
-    public GameObject MainUI;//메인 UI?�당
-    private List<Slot> slotList = new List<Slot>();//Slot?�을 리스???�당
+    private List<Slot> slotList = new List<Slot>();//
 
     //private Button moveUI;
 
@@ -26,84 +26,134 @@ public class EventManager : MonoBehaviour
     }
     private void Start() 
     {
+        #region SmithInventoryInit
         for(int i = 0; i < SmithInventory.GetComponent<Inventory>().slots.Length; i++)
-
         {
             int index = i;
-            
             Slot temp = new Slot();
-
             temp.gameObject = SmithInventory.transform.GetChild(1).GetChild(0).GetChild(i).GetChild(1).gameObject;
             temp.ChooseSlot = false;
             slotList.Add(temp);
             slotList[i].gameObject.GetComponent<Button>().onClick.AddListener(() => InvenBtnChoise(index));
         } 
+        #endregion
+        #region MainUIInit
 
-        Button[] MainUIButtonList = MainUI.GetComponentsInChildren<UnityEngine.UI.Button>();//MainUI버튼?�당
-
+        Button[] MainUIButtonList = MainUI.GetComponentsInChildren<UnityEngine.UI.Button>();//MainUI버튼
         for(int i = 0; i < MainUIButtonList.Length; i++)
         {
             int index = i;
             MainUIButtonList[i].onClick.AddListener(() => MainUiControll(index, MainUIButtonList.Length));
         }
+        
+        #endregion
+        
+        #region User_PanelInit
+        Button[] UserPanelBtnList = UserPanel.GetComponentsInChildren<UnityEngine.UI.Button>();
+        for(int j = 0; j < UserPanelBtnList.Length; j++)
+        {
+            int index = j;
+            UserPanelBtnList[j].onClick.AddListener(() => UserPanelControll(index, gameCanvas.transform.childCount));
+        }
 
-        gameCanvas.transform.GetChild(2).gameObject.SetActive(true); //?�작??메인?�면 On
-        gameCanvas.transform.GetChild(1).GetChild(0).gameObject.SetActive(false); //?�작??메인?�면 On
+        #endregion
 
+        gameCanvas.transform.GetChild(2).gameObject.SetActive(true); //
+        gameCanvas.transform.GetChild(1).GetChild(0).gameObject.SetActive(false); //
+        
+        
         
     }
     
-
+    #region ChooseInvenItem
     Color AlphaColorChange(int i, float Value)
     {
-        Color color = slotList[i].gameObject.GetComponent<UnityEngine.UI.Image>().color;//?�재 버튼 ?�깔?�자�??�달받기
+        Color color = slotList[i].gameObject.GetComponent<UnityEngine.UI.Image>().color;//
         color.a = Value;
         slotList[i].gameObject.GetComponent<UnityEngine.UI.Image>().color = color;
         return slotList[i].gameObject.GetComponent<UnityEngine.UI.Image>().color;
     }
-
-    void CleanUi(int UiLength)
-    {
-        for(int i = 0; i < UiLength-1; i++)
-        {
-            gameCanvas.transform.GetChild(i+2).gameObject.SetActive(false);
-        }
-        gameCanvas.transform.GetChild(1).GetChild(0).gameObject.SetActive(true);
-    }
-
-    void MainUiControll(int UiId, int UiLength)
-    {
-        CleanUi(UiLength);
-        gameCanvas.transform.GetChild(UiId+3).gameObject.SetActive(true);
-    }
-
+    
     void CleanSlots()
     {
         for(int i = 0; i < SmithInventory.GetComponent<Inventory>().slots.Length; i++)
         {
             AlphaColorChange(i, 0.0f);
-            slotList[i].ChooseSlot = false;//?�등 X
-        } 
+            slotList[i].ChooseSlot = false;//
+        }
+        ItemDetailShow(false);
     }
-
-
     void InvenBtnChoise(int buttonId)
     {
-        if(slotList[buttonId].ChooseSlot) 
-        {
-            CleanSlots();
-            return;
-        }
-        else
-        {
-            CleanSlots();
-            slotList[buttonId].ChooseSlot = true;
-        }
         
-        if(slotList[buttonId].ChooseSlot)
+        List<Item> itemlist = SmithInventory.GetComponent<Inventory>().items;
+        if(itemlist[buttonId])
         {
-            AlphaColorChange(buttonId, 0.3f);
+            ItemDetailShow(false);
+            if(slotList[buttonId].ChooseSlot) 
+
+            {
+                CleanSlots();
+                return;
+            }
+            else
+            {
+                CleanSlots();
+                slotList[buttonId].ChooseSlot = true;
+            }
+        
+            if(slotList[buttonId].ChooseSlot)
+            {
+                AlphaColorChange(buttonId, 0.3f);
+                ItemDetailShow(true);
+            }
         }
+        else return;
+    }
+    #endregion
+    #region MainUI
+    
+    void ChangeMainUI(int UiLength, bool isShow)
+    {
+        for(int i = 0; i < UiLength-2; i++)
+        {
+            gameCanvas.transform.GetChild(i+2).gameObject.SetActive(false);
+        }
+        gameCanvas.transform.GetChild(1).GetChild(0).gameObject.SetActive(isShow);
+        ItemDetailShow(false);
+        
+    }
+
+    void MainUiControll(int index, int UiLength)
+    {
+        ChangeMainUI(UiLength, true);
+        gameCanvas.transform.GetChild(index+3).gameObject.SetActive(true);
+    }
+    #endregion
+    
+
+
+   
+
+    void UserPanelControll(int index, int Length)
+    {
+        if(index == 0)
+        {
+            CleanSlots();
+            ChangeMainUI(Length, false);
+            gameCanvas.transform.GetChild(2).gameObject.SetActive(true); //
+        }
+    }
+
+    void ItemDetailShow(bool isShow)
+    {
+        GameObject ItemAbility = gameCanvas.transform.GetChild(4).GetChild(0).GetChild(1).GetChild(0).GetChild(0).gameObject;
+        GameObject NpcTalkBollum = gameCanvas.transform.GetChild(4).GetChild(0).GetChild(1).GetChild(0).GetChild(1).GetChild(0).gameObject;
+        GameObject NpcText = gameCanvas.transform.GetChild(4).GetChild(0).GetChild(1).GetChild(0).GetChild(1).GetChild(1).gameObject;
+        ItemAbility.SetActive(isShow);
+        NpcTalkBollum.SetActive(isShow);
+        NpcText.SetActive(isShow);
+        
     }
 }
 
