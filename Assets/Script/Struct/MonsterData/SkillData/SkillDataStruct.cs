e5977c813a0b0d49235f0c89a8861b310ffc28db
Razack <wsxed1230@gmail.com>
2024-03-21 13:53:58 +0900

    0321 중간저장
    몬스터 팩토리 클래스 수정

--- Assets/Script/Struct/MonsterData/SkillData/SkillDataStruct.cs
+++ Assets/Script/Struct/MonsterData/SkillData/SkillDataStruct.cs
@@ -1,7 +1,7 @@
 public struct SkillDataStruct
 {
     public int Index;
-    public int Skill_Name;
+    public string Skill_Name;
     public float Skill_PreDelay;
     public float Skill_PostDelay;
     public float Skill_DetectRange;
@@ -15,20 +15,44 @@ public struct SkillDataStruct
 
 public struct SkillMovementTypeDataStruct
 {
-
+    public int Index;
+    public float Skill_ShortRangeAttackSpeed;
+    public float Skill_ShortRangeAttackDist;
 }
 
 public struct SkillMeleeTypeDataStruct
 {
-
+    public int Index;
+    public int Skill_NumOfHitBox;
+    public int Skill_HitBox;
+    public float Skill_hitDuration;
+    public int Skill_AffectOption1;
+    public int Skill_AffectOption2;
+    public int Skill_AffectOption3;
+    public int Skill_AffectOption4;
+    public int Skill_AffectOption5;
 }
 
 public struct SkillProjectileTypeDataStruct
 {
-
+    public int Index;
+    public int Skill_NumOfHitBox;
+    public int Skill_HitBox;
+    public int Skill_Unpenetrable;
+    public float Skill_hitDuration;
+    public float Skill_LongRangeAttackSpeed;
+    public float Skill_LongRangeAttackDist;
+    public bool Skill_Penetrable;
+    public int Skill_AffectOption1;
+    public int Skill_AffectOption2;
+    public int Skill_AffectOption3;
+    public int Skill_AffectOption4;
+    public int Skill_AffectOption5;
 }
 
-public struct SkillAffectDataStruct
+public struct SkillDamageAffectDataStruct
 {
-
+    public int Index;
+    public float Skill_Power;
+    public int Skill_AttackType;
 }
--- Assets/Script/Unit/Mob/MonsterFactory.cs
+++ Assets/Script/Unit/Mob/MonsterFactory.cs
@@ -1,18 +1,111 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Newtonsoft.Json;
 
 public class MonsterFactory : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static void CreateMonsterSkill(GameObject parent ,int index)
     {
-        
+        GameObject obj = new GameObject();
+        SkillDataStruct data = default;
+
+        //load from file
+        var json = Resources.Load<TextAsset>("Monster/SkillData/Monster_SkillData").text;
+        var arrMonsterSkillDatas = JsonConvert.DeserializeObject<SkillDataStruct[]>(json);
+        foreach(var skillData in arrMonsterSkillDatas)
+        {
+            if(skillData.Index == index)
+            {
+                data = skillData;
+                break;
+            }
+        }
+
+        //making skill by recipe
+        obj.name = data.Skill_Name;
+        Skill objSkill = obj.AddComponent<Skill>();
+        objSkill.preDelay = data.Skill_PreDelay;
+        objSkill.postDelay = data.Skill_PostDelay;
+        objSkill.detectRadius = data.Skill_DetectRange;
+        objSkill.targetMask = 1 << data.Skill_TargetMask;
+
+        AddSkillType(objSkill, data.Skill_Option1);
+        AddSkillType(objSkill, data.Skill_Option2);
+        AddSkillType(objSkill, data.Skill_Option3);
+        AddSkillType(objSkill, data.Skill_Option4);
+
+
+        objSkill.transform.SetParent(parent.transform);
     }
 
-    // Update is called once per frame
-    void Update()
+    public static void AddSkillType(Skill parent, int index)
     {
-        
+        GameObject obj = new GameObject();
+
+        switch (index / 10000)
+        {
+            //MovementSkillType
+            case 1:
+                {
+                    SkillMovementTypeDataStruct data = default;
+                    //load from file
+                    var json = Resources.Load<TextAsset>("Monster/SkillData/SkillType/Monster_SkillType_Movement").text;
+                    var arrDatas = JsonConvert.DeserializeObject<SkillMovementTypeDataStruct[]>(json);
+                    foreach (var Data in arrDatas)
+                    {
+                        if (Data.Index == index)
+                        {
+                            data = Data;
+                            break;
+                        }
+                    }
+                    MovementSkillType move = obj.AddComponent<MovementSkillType>();
+                    move.maxDist = data.Skill_ShortRangeAttackDist;
+                    move.moveSpeed = data.Skill_ShortRangeAttackSpeed;
+                }
+                break;
+            //MeleeSkillType
+            case 2:
+                {
+                    SkillMeleeTypeDataStruct data = default;
+                    //load from file
+                    var json = Resources.Load<TextAsset>("Monster/SkillData/SkillType/Monster_SkillType_Melee").text;
+                    var arrDatas = JsonConvert.DeserializeObject<SkillMeleeTypeDataStruct[]>(json);
+                    foreach (var Data in arrDatas)
+                    {
+                        if (Data.Index == index)
+                        {
+                            data = Data;
+                            break;
+                        }
+                    }
+                }
+                break;
+            //ProjectileSkillType
+            case 3:
+                {
+                    SkillProjectileTypeDataStruct data = default;
+                    //load from file
+                    var json = Resources.Load<TextAsset>("Monster/SkillData/SkillType/Monster_SkillType_Projectile").text;
+                    var arrDatas = JsonConvert.DeserializeObject<SkillProjectileTypeDataStruct[]>(json);
+                    foreach (var Data in arrDatas)
+                    {
+                        if (Data.Index == index)
+                        {
+                            data = Data;
+                            break;
+                        }
+                    }
+
+
+
+                    obj.transform.SetParent(parent.transform);
+                }
+                break;
+            //dont have skill type
+            default:
+                return;
+        }
+
+        obj.transform.SetParent(parent.transform);
     }
 }
