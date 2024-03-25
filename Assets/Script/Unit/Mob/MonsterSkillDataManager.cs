4b8934d98793d00659a90f8f74b8dd81d11193cc
YongJinKing <dydwls120@gmail.com>
2024-03-25 23:22:30 +0900

    0325 보류작업

---
+++ Assets/Script/Unit/Mob/MonsterSkillDataManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json;
+using System.Linq;
+
+public class MonsterSkillDataManager
+{
+    private static MonsterSkillDataManager instance;
+}
+
