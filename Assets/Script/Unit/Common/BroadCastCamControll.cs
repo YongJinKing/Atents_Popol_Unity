42cb320022b814c7fec732c5a76254d73f56fcaf
LOLPeterPan <lkjhg1642@gmail.com>
2024-03-31 23:06:39 +0900

    Brodcast Position Sample

--- Assets/Script/Unit/Common/BroadCastCamControll.cs
+++ Assets/Script/Unit/Common/BroadCastCamControll.cs
@@ -4,19 +4,41 @@ using UnityEngine;
 
 public class BroadCastCamControll : MonoBehaviour
 {
-    public Transform PlayerCam;
-    public Transform MonsterCam;
-    Collider[] colliders;
-    LayerMask player;
-    LayerMask Monster;
+    public LayerMask playerLayer;
+    public LayerMask monsterLayer;
+
+    public GameObject playerObject;
+    public GameObject monsterObject;
+
+    public Vector3 playerOffset;
+    public Vector3 monsterOffset;
+
+    public Transform plCam;
+    public Transform moCam;
 
     void Start()
     {
-        colliders = Physics.OverlapSphere(transform.position, 10f, player);
+        Collider[] playerColliders = Physics.OverlapSphere(transform.position, 100f, playerLayer);
+        if (playerColliders.Length > 0)
+        {
+            playerObject = playerColliders[0].gameObject;
+        }
+
+        Collider[] monsterColliders = Physics.OverlapSphere(transform.position, 100f, monsterLayer);
+        if (monsterColliders.Length > 0)
+        {
+            monsterObject = monsterColliders[0].gameObject;
+        }
     }
     
     void Update()
     {
-        
+        transform.position = playerObject.transform.position + (monsterObject.transform.position - playerObject.transform.position) / 2;
+
+        plCam.transform.position = playerObject.transform.position + playerOffset;
+        moCam.transform.position = monsterObject.transform.position + monsterOffset;
+
+        plCam.transform.LookAt(playerObject.transform);
+        moCam.transform.LookAt(monsterObject.transform);
     }
 }
