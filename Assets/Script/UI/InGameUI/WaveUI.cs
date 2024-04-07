0fa44067db6a9a8eddbaf9c1476e9ebcb549f516
LOLPeterPan <lkjhg1642@gmail.com>
2024-04-08 00:28:50 +0900

    WaveUI ing

--- Assets/Script/UI/InGameUI/WaveUI.cs
+++ Assets/Script/UI/InGameUI/WaveUI.cs
@@ -1,18 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class WaveUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    int nowWave = 1;
+    public int TotalWave;
+    public GameObject WaveTxt;
+    public GameObject WaveNum;
+
+    Vector2 txtPos;
+    public Vector2 txtEndPos;
+
+    float txtScale = 0.44f;
+    public float txtEndScale;
+
+    public float moveSpeed;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Keypad0))
+        {
+            StartCoroutine(WaveChange());
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            WaveNum.GetComponent<TMP_Text>().text = "1";
+        }
+    }
+
+    int state = 0;
+    IEnumerator WaveChange()
+    {
+        while (state == 0)      //Move to Center
+        {
+            txtPos.y = Mathf.Lerp(txtPos.y, txtEndPos.y, Time.deltaTime * moveSpeed);
+            txtScale = Mathf.Lerp(txtScale, txtEndScale, Time.deltaTime * moveSpeed);
+            UIMove();
+            if (Mathf.Floor(Vector2.Distance(txtPos, txtEndPos)) == 0)
+            {
+                ++state;
+            }
+            yield return null;
+        }
+        float alph = 1.0f;
+        while (state == 1)      //Change num
+        {
+            alph -= Time.deltaTime * moveSpeed;
+            WaveTxt.GetComponent<TMP_Text>().alpha = Mathf.Abs(alph);
+            if (alph >=0)
+            {
+                yield return null;
+            }
+
+        }
+    }
+
+    private void UIMove()
+    {
+        WaveTxt.GetComponent<RectTransform>().position = txtPos;
+        WaveTxt.GetComponent<RectTransform>().localScale = new Vector3(txtScale, txtScale, 1);
     }
 }
