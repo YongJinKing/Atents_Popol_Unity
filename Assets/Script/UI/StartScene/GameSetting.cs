368c7bb7c71d2fe181b5190eedaf10dd947866ba
YongJinKing <dydwls120@gmail.com>
2024-04-17 23:54:10 +0900

    0417 설정UI 구성

--- Assets/Script/UI/StartScene/GameSetting.cs
+++ Assets/Script/UI/StartScene/GameSetting.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameSetting : MonoBehaviour
 {
+    public Sprite Img_Sound;
+    public Sprite Img_Mute;
+    public GameObject Obj_Bgm;
+    public GameObject Obj_Sfx;
     // Start is called before the first frame update
     void Start()
     {
